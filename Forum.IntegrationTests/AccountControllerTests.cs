32aec9a9483d743ecf2e1257137118342cbec0b5
Kamil Wolak <kamil.wolak@interia.eu>
2022-08-15 17:19:47 +0200

    Added tests for LoginAccount

--- Forum.IntegrationTests/AccountControllerTests.cs
+++ Forum.IntegrationTests/AccountControllerTests.cs
@@ -2,10 +2,12 @@
 using Forum.Entities;
 using Forum.IntegrationTests.Helper;
 using Forum.Models;
+using Microsoft.AspNetCore.Authorization.Policy;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +21,7 @@ namespace Forum.IntegrationTests
     public class AccountControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _client;
-
+        private readonly Mock<IAccountService> _mock = new Mock<IAccountService>();
         public AccountControllerTests()
         {
             _client = new WebApplicationFactory<Program>()
@@ -31,7 +33,7 @@ namespace Forum.IntegrationTests
                         services.Remove(dbContext);
 
                         services.AddDbContext<ForumDbContext>(options => options.UseInMemoryDatabase("ForumDb"));
-
+                        
 
                     });
                 }).CreateClient();
@@ -73,5 +75,51 @@ namespace Forum.IntegrationTests
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
+        [Fact]
+        public async Task Login_ForValidModel_ReturnsOk()
+        {
+            //arrange
+             _mock.Setup(x => x.GenerateJWT(It.IsAny<LoginDto>()))
+                .ReturnsAsync("jwt");
+            var model = new LoginDto()
+            {
+                Nick = "Test",
+                Password = "test1"
+            };
+            //act
+            var httpContent = model.SerializeForHttp();
+            var response = await _client.PostAsync("api/account/login", httpContent);
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+        [Fact]
+        public async Task Login_ForInvalidModel_ReturnsBadRequest()
+        {
+            //arrange
+            _mock.Setup(x => x.GenerateJWT(It.IsAny<LoginDto>()))
+               .ReturnsAsync("jwt");
+            var model = new LoginDto()
+            {
+                Nick = "Test"
+                
+            };
+            //act
+            var httpContent = model.SerializeForHttp();
+            var response = await _client.PostAsync("api/account/login", httpContent);
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+        [Fact]
+        public async Task GetAll_ForRegisteredAdmin_ReturnsOk()
+        {
+            var response = await _client.GetAsync("api/accounts");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        }
+
     }
 }
